6a450ed03d5221c5961dc72cb5749a8e9837fca4
olaolatunbos <olaolat@hotmail.com>
2021-08-05 14:13:11 +0100

    Crypto App

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:crypto_app/blocs/crypto/crypto_bloc.dart';
+import 'package:crypto_app/repositories/crypto_repository.dart';
+import 'package:crypto_app/screens/home_screen.dart';
+
+void main() {
+  EquatableConfig.stringify = kDebugMode;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => CryptoRepository(),
+      child: MaterialApp(
+        title: 'Flutter Crypto App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.black,
+          accentColor: Colors.tealAccent,
+        ),
+        home: BlocProvider(
+          create: (context) => CryptoBloc(
+            cryptoRepository: context.read<CryptoRepository>(),
+          )..add(AppStarted()),
+          child: HomeScreen(),
+        ),
+      ),
+    );
+  }
+}
